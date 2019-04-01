24d713b698853b6538a0c944ce5a42edca7a533d
Nabil <nabilshaahani@gmail.com>
2019-04-01 23:41:19 +0400

    Adding Feature

---
+++ app.js
@@ -0,0 +1,21 @@
+// Get all the DOM elements and save it to variable
+
+// function messageBox(){
+
+
+// }
+
+
+
+
+
+// First Part
+// let player = document.getElementById('name-c');
+// let UserName = prompt("Enter Your name");
+// player.innerHTML = UserName;
+
+// Second Part
+function comClick() {
+	alert("Please select from your section");
+}
+// Third Part
