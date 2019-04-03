efdecb2f6c45f0c46e419de349121da1c36e26e9
Nabil <nabilshaahani@gmail.com>
2019-04-03 21:15:19 +0400

    updated version1

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const scissorU = document.getElementById('scissor-u');
 
 const imageU = document.getElementById('user-choice');
 const comRstU = document.getElementById('user-result');
+const scoreU = document.getElementById('score-u');
 
 // com
 const rockC = document.getElementById('rock-c');
@@ -14,17 +15,39 @@ const scissorC = document.getElementById('scissor-c');
 
 const imageC = document.getElementById('com-choice');
 const comRstC = document.getElementById('com-result');
+const scoreC = document.getElementById('score-c');
 
 //common variable
 const resultM = document.getElementById('in-rst');
+const middle = document.getElementById('middle');
+const resultR = document.getElementById('reset-rst');
+const end = document.getElementById('re-start');
+const footer = document.getElementById('footer');
+const newPlayer = document.getElementById('new-player');
+
 // ************************************************************************************ //
 
 // First Part
 let player = document.getElementById('name-c');
-let UserName = prompt("Enter Your name");
+var UserName = prompt("Enter Your name");
 player.innerHTML = UserName;
 
 
+
+function nameChecker()
+{
+	// if (UserName === undefined) {
+	// 	let UserName = prompt("Enter Your name");
+	// }
+	// else{
+		if (UserName.toUpperCase() == "NUJI" || UserName.toUpperCase() == "NUZA" ) {
+			resultM.innerHTML = "I love you So much " + UserName + ", I am blessed to have you in my life";
+		}
+		else{
+			resultM.innerHTML = "Welcome "+ UserName;
+		}
+	// }
+}
 
 
 // Enter your name
@@ -32,7 +55,8 @@ function comClick() {
 	alert("Please select from your section");
 }
 
-
+let comScore = 0;
+let userScore = 0;
 
 // com Part
 function comPlay() {
@@ -71,34 +95,45 @@ function playGround(userSelection, comSelection) {
 	else if(userSelection == 'rock') {
 
 		if (comSelection == 'scissor') {
+			userScore++;
 			resultM.innerHTML = "You Win! Rock beats scissors.";
+			scoreU.innerHTML = userScore;
 		} 
 		else{
+			comScore++
 			resultM.innerHTML = "You Lose! Paper beats rock.";
+			scoreC.innerHTML =comScore;
 		}
 	} 
 
 	else if (userSelection == 'paper') {
 
 		if (comSelection == 'rock') {
+			userScore++;
 			resultM.innerHTML = "You Win! Paper beats rock";
+			scoreU.innerHTML = userScore;
 		} 
 		else{
+			comScore++;
 			resultM.innerHTML = "You Lose! Scissors beat paper.";
+			scoreC.innerHTML = comScore;
 		}
 	} 
 
 	else{
 		if (comSelection == 'paper') {
+			userScore++;
 			resultM.innerHTML = "You Win! Scissors beat paper.";
+			scoreU.innerHTML = userScore;
 		} 
 		else{
+			comScore++;
 			resultM.innerHTML = "You Lose! Rock beats scissors.";
+			scoreC.innerHTML = comScore;
 		}
 	}
 
 }
-
 
 
 
@@ -122,8 +157,20 @@ function game(userChoice) {
 
 	let comp = comPlay();
 	playGround(player, comp);
-}
 
+
+	if (userScore == 5) {
+		middle.style.display = "none";
+		footer.style.display = "block";
+		resultR.innerHTML = "You Defeated The Computer By " + (userScore - comScore);
+	} else if (comScore == 5) {
+		middle.style.display = "none";
+		footer.style.display = "block";
+		resultR.innerHTML = "The Computer Defeated You By " + (comScore - userScore);
+	}
+
+}
+nameChecker();
 
 
 // EventListener[accordingly user selection]
@@ -138,3 +185,21 @@ paperU.addEventListener("click", function() {
 scissorU.addEventListener("click", function() {
 			game('scissor');
 		});
+
+end.addEventListener('click', function(){
+	comScore = 0;
+	userScore = 0;
+	scoreU.innerHTML = "0";
+	scoreC.innerHTML = "0";
+	imageU.setAttribute("src" ,"");
+	comRstU.innerHTML = UserName + " Play Again";
+	imageC.setAttribute("src" ,"");
+	comRstC.innerHTML = "Let's Play Again";
+	resultM.innerHTML = "Play Again";
+	middle.style.display = "block";
+	footer.style.display = "none";
+})
+
+newPlayer.addEventListener('click', function(){
+	location.reload();
+})
